f299c9c1b342c22c9d9ca2864418038bcd14b610
musawakiliML <musawakiliml@hotmail.com>
2022-08-03 21:25:52 +0100

    Added file upload and pdf file extract

--- Fetch_Meta_Data_App/views.py
+++ Fetch_Meta_Data_App/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Fetch_Meta_Data_App.utils_functions.functions import handle_uploaded_file
+from Fetch_Meta_Data_App.utils_functions.extract_meta_data import get_pdf_metadata
 from .forms import FileUpload
 
 # Fetch metadata packages
@@ -9,11 +10,16 @@ from .forms import FileUpload
 
 # Create your views here.
 def upload_file(request):
+    meta_data = ""
     if request.method == 'POST':
         form = FileUpload(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['user_file'])
-            return HttpResponse('File Uploaded Succesfully!')
+            meta_data = get_pdf_metadata(request.FILES['user_file'])
+            # print(meta_data)
+            # return HttpResponse('File Uploaded Succesfully!')
+            return HttpResponse(meta_data)
     else:
         form = FileUpload()
-    return render(request, 'index.html', {'form': form})
+    context = {"metadata": meta_data, 'form': form}
+    return render(request, 'index.html', context)  # {'form': form})
