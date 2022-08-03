89b1fed23f6aaac5e1a39ca108788afc25281b0f
musawakiliML <musawakiliml@hotmail.com>
2022-08-03 18:00:49 +0100

    File upload and utils Functions

--- Fetch_Meta_Data_App/views.py
+++ Fetch_Meta_Data_App/views.py
@@ -1,3 +1,19 @@
+# Admin libraries
 from django.shortcuts import render
+from django.http import HttpResponse
+from Fetch_Meta_Data_App.utils_functions.functions import handle_uploaded_file
+from .forms import FileUpload
+
+# Fetch metadata packages
+
 
 # Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUpload(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['user_file'])
+            return HttpResponse('File Uploaded Succesfully!')
+    else:
+        form = FileUpload()
+    return render(request, 'index.html', {'form': form})
