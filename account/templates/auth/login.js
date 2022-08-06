12d7cde64c01dcdcbeeb01392f3ca5c568eedce1
Ultra-tech-code <hizick27@gmail.com>
2022-08-07 00:41:12 +0100

    edited to meet with the final design

--- account/templates/auth/login.js
+++ account/templates/auth/login.js
@@ -1,5 +1,7 @@
 const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
+const changeBorder = document.getElementsByTagName("INPUT");
+console.log(changeBorder)
 
 togglePassword.addEventListener("click", function () {
     // toggle the type attribute
@@ -8,5 +10,20 @@ togglePassword.addEventListener("click", function () {
     
     // toggle the icon
     this.classList.toggle("bi-eye");
+
 });
+// bi bi-eye-slash
+
+for (const input of changeBorder) {
+    input.addEventListener("mouseout", function(){
+        if(changeBorder.value.length > 0){
+            this.style.borderColor = "black";
+        }
+    });
+
+};
+
+
+
+
 
